fafa6b5f8d44812ad36647a794713b1ea058325d
Jenny Hu <jenhu@sas.upenn.edu>
2014-01-20 13:51:09 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HW1961::Application.config.secret_key_base = '5f7f9eed49c36beae01ad94f1b79f1e09a94e206b5bc9e9e4cfa1386ebb690d73dc61e20ea946dcf00df5ebdbd6da3ad1a2a5767ef6773b3e19065d317ed52f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HW1961::Application.config.session_store :cookie_store, key: '_HW1961_session'
